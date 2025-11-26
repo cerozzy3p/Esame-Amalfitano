79e1263c473466847951a12352d983f9b267b8f7
cerozzy3p <gxk56bng46@privaterelay.appleid.com>
2025-11-26 23:23:17 +0100

    Delete ComparatorPerTarga.java

--- ComparatorPerTarga.java
+++
@@ -1,9 +0,0 @@
-package EsercitazionePreEsame;
-
-import java.util.Comparator;
-
-public class ComparatorPerTarga implements Comparator<Veicolo> {
-    public int compare(Veicolo v1, Veicolo v2) {
-        return v1.getTarga().compareTo(v2.getTarga());
-    }
-}
